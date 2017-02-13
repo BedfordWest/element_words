20d4ebe59cbc83c99471e5841a3417c9e009de18
Bedford <bedford@Bedfords-MacBook-Air.local>
2017-02-12 20:32:51 -0600

    feat(words): Check in of a functioning version with basic rules

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use std::io;
+use std::cmp::Ordering;
 
 extern crate csv;
+extern crate rustc_serialize;
 
 fn main() {
-    type Row = (i8, String, String, f32);
+    
+    #[derive(RustcDecodable, Debug, Clone)]
+    struct Row {
+        number: i8,
+        symbol: String,
+        full_name: String,
+        weight: f32,
+    }
+
+    impl Eq for Row {}
+        
+    impl Ord for Row {
+        fn cmp(&self, other: &Row) -> Ordering {
+            self.number.cmp(&other.number)
+        }
+    }
+
+    impl PartialOrd for Row {
+        fn partial_cmp(&self, other: &Row) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl PartialEq for Row {
+        fn eq(&self, other: &Row) -> bool {
+            self.number == other.number
+        }
+    }
 
     let mut rdr = csv::Reader::from_file("./ptdata2.csv").unwrap();
-    let rows = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
+    let rows: Vec<Row> = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
     let mut accepted_rows: Vec<Row> = vec![];
+
     for row in &rows {
-        println!("Element {}: {}, {} weighs {}", row.0, row.1, row.2, row.3);
+        println!("Element {}: {}, {} weighs {}", row.number, row.symbol, row.full_name, row.weight);
     }
     
     loop {
@@ -18,14 +48,30 @@ fn main() {
             Ok(_) => {
                 input = input.trim().to_string();
                 println!("{}", input);
+                input = input.to_lowercase();
+                println!("{}", input);
                 if input == "exit" {
                     break;
                 }
 
                 for row in &rows {
-                    let v: Vec<_> = input.match_indices(&row.1).collect();
-                    println!("{:?}", v);
+                    let new_v: Vec<_> = input.match_indices(&row.symbol.to_lowercase()).collect();
+                    println!("{:?}", new_v);
+                    if new_v != [] {
+                        let mut new_row: Row = row.clone();
+                        accepted_rows.push(new_row);
+                    }
                 }
+                accepted_rows.sort();
+                let mut symbols: String = String::from("");
+                let mut names: String = String::from("");
+                for element in &accepted_rows {
+                    symbols.push_str(&element.symbol);
+                    names.push_str(&element.full_name);
+                }
+
+                println!("{}", symbols);
+                println!("{}", names);
             }
             Err(error) => println!("error: {}", error),
         }
